f55c9306ee28d32a91716f3fa3509de4920a1e28
MSgun7 <fgarr@naver.com>
2023-03-27 21:55:16 +0900

    Update Character.py
    Make the battle

--- Character.py
+++ Character.py
@@ -40,18 +40,20 @@ class Player(Character):
     # print("공격력과 마력이 갱신되었습니다.")
 
     # 마법 공격에 대한 코드입니다.
-    def mattack(self, enemy):
-        damage = random.randint(self.mpower - 2, self.mpower + 2)
+    def mattack(self, enemy, mp=20):
+        self.mp = self.mp - mp
+        damage = random.randint(self.mpower - 200, self.mpower + 400)
         enemy.hp = max(enemy.hp - damage, 0)
         print(f"{self.name}의 마법공격! {enemy.name}에게 {damage}의 마법 데미지를 입혔습니다.")
         if enemy.hp == 0:
             print(f"{enemy.name}이(가) 쓰러졌습니다.")
-
+            if self.mp <= 0:
+                print(f"MP를 모두 사용하셨습니다.")
 
     # 부모 클래스에 존재하는 status_check 메소드를 overriding 합니다.
 
     def status_check(self):
-        print(f"Player's hp : {self.hp}")
+        print(f"Player's hp : {self.hp}, Player's mp : {self.mp}")
 
 
 class Monster(Character):
@@ -76,24 +78,31 @@ devils = [
     Monster("디아블로", 4000, 30)
 ]
 devil = random.choice(devils)
-
+print("불타오르는 대지 그 너머 당신의 눈 앞에 힘의 문양이 떠올랐습니다.")
+print(f"{player.name}님 당신이 맞설 상대는 군주라 불리는 악마인 {devil.name} 입니다. 너무 늦기 전에 그 차원문을 타고 가서 {devil.name}를 쓰러트려 주십시오!")
 
 
 while True:
-
-    print(f"{player.name}님 악마 {devil.name}가(이) 소환되었습니다. 전투를 준비하세요.")
-    Player.show_status(player)
-    Monster.status_check(devil)
-    # print(f"{player.name}님의 체력을 확인합니다. : {player.hp}")
-    # print(f"{devil.name}의 체력을 확인합니다. : {devil.hp} 더러운 악마에게 신의 철퇴를")
-    
-    
-
+    # 최초 스테이터스 확인
+    # Player.show_status(player)
+    # Monster.show_status(devil)
+    # 공격 방식 선택
+    print("공격 방법을 선택해주세요.")
+    select_attack = int(input("1. 통상공격 2. 마법공격"))
     devil.attack(player)
-
     Player.show_status(player)
-    Monster.status_check(devil)
-    
-    break
-    
-    
+    Monster.show_status(devil)
+
+    if select_attack == 1:
+        player.attack(devil)
+    else:
+        player.mattack(devil)
+        print(f"{player.name}의 상태: MP {player.mp}")
+    if devil.hp == 0:
+        print(f"{devil.name}이(가) 쓰러졌습니다. 당신의 승리입니다. 저희는 구원받을 것입니다.")
+        print("Game clear")
+        break
+    elif player.hp == 0:
+        print(f"{player.name}이(가) 쓰러졌습니다. 당신의 죽음으로 인해 악마는 지옥을 벗어나 지상으로 향합니다.")
+        print("You die")
+        break
