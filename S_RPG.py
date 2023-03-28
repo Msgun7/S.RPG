1333fb370db7aa2fdeb2786a30cc7c4c3d159674
MSgun7 <fgarr@naver.com>
2023-03-28 16:02:21 +0900

    d
    d

--- S_RPG.py
+++ S_RPG.py
@@ -1,5 +1,4 @@
 import random
-from random import choice
 import time
 
 
@@ -35,29 +34,43 @@ class Player(Character):
 
     # 플레이어의 통상공격
     def attack(self, enemy):
-        damage = random.randint(self.power - 20, self.power + 20)
+        damage = random.randint(self.power - 2, self.power + 2)
         enemy.hp = max(enemy.hp - damage, 0)
         print(f"\033[32m{player_name}\033[0m의" + "\033[96m{통상공격}\033[0m 으로" +
               f"\033[31m{devil.name}\033[0m에게 \033[95m{damage}\033[0m의 데미지를 입혔습니다.")
+        print("악마는 "+"\033[96m{통상공격}\033[0m"+"에 큰 내성을 갖고 있습니다. 그들의 약점인 " +
+              "\033[93m{신성마법}\033[0m으로 큰 데미지를 입히세요.")
         if enemy.hp == 0:
             print(f"\033[31m{devil.name}\033[0m가(이) 쓰러졌습니다.")
+
+    def resurrect(self, enemy):
+        choice = int(
+            input("쓰러진 당신에게 가호가 내려집니다. 제발 저희를 구원해주세요. (1: 부활 / 2: 안식 )"))
+        if choice == 1:
+            self.hp = self.max_hp
+            print(f"\033[31m{player_name}\033[0m 님 감사합니다. 다시 한번 구원의 손길을!")
+            enemy.attack(self)
+        else:
+            print("안식을 선택하셨군요. 무리한 부탁을 들어주시느라 수고하셨습니다. 당신을 나무라지 않겠습니다. 부디 편히 잠드시길..")
 
 # 플레이어의 마법공격
     def mattack(self, enemy):
         if self.mp == 0:
             print("마나가 부족하여" + f"\033[32m{self.name}\033[0m" +
-                  "님의 "+"\033[93m{마법공격}\033[0m이 사용불가능합니다.")
+                  "님의 "+"\033[93m{신성마법}\033[0m이 사용불가능합니다.")
             return
 
         self.mp = self.mp - 40
-        damage = random.randint(self.mpower - 200, self.mpower + 400)
+        damage = random.randint(self.mpower - 500, self.mpower + 1000)
         enemy.hp = max(enemy.hp - damage, 0)
-        print(f"\033[32m{self.name}\033[0m" + "의 \033[93m{마법공격}\033[0m ! " +
-              f"\033[31m{enemy.name}\033[0m" + f"에게 \033[95m{damage}\033[0m의 마법 데미지를 입혔습니다.")
+        print(f"\033[32m{self.name}\033[0m" + "의 \033[93m{신성마법}\033[0m ! " +
+              f"\033[31m{enemy.name}\033[0m" + f"에게 \033[95m{damage}\033[0m의" + "\033[93m{신성마법}\033[0m 데미지를 입혔습니다.")
         if enemy.hp == 0:
             print(f"\033[31m{enemy.name}\033[0m" + "이(가) 쓰러졌습니다.")
         if self.mp <= 0:
-            print("MP를 모두 사용하셨습니다.")
+            print("MP를 모두 사용하셨습니다. 축복으로 회복시키겠습니다. ")
+            # mp를 160 회복, 최대값을 넘지 않도록 함
+            self.mp = min(self.mp + 160, self.mp_max)
 
     def show_status(self):
         print(
@@ -76,9 +89,9 @@ class Monster(Character):
 
     # 몬스터의 통상공격
     def attack(self, enemy):
-        damage = random.randint(self.power - 20, self.power + 20)
+        damage = random.randint(self.power - 2, self.power + 2)
         enemy.hp = max(enemy.hp - damage, 0)
-        print(f"\033[31m{devil.name}\033[0m의" + "\033[96m{통상공격}\033[0m 으로" +
+        print(f"\033[31m{devil.name}\033[0m의" + "\033[96m{통상공격}\033[0m" "으로" +
               f"\033[32m{player_name}\033[0m" + f"에게 \033[95m{damage}\033[0m의 데미지를 입혔습니다.")
         if enemy.hp == 0:
             print(f"\033[32m{player_name}\033[0m가(이) 쓰러졌습니다.")
@@ -92,19 +105,46 @@ class Monster(Character):
               f" : \033[91m{self.hp}\033[0m")
 
 
+class Diablo(Monster):
+    def __init__(self, name, hp, power):
+        super().__init__(name, hp, power)
+
+    def attack(self, enemy):
+        damage = random.randint(self.power - 20, self.power + 200)
+        enemy.hp = max(enemy.hp - damage, 0)
+        print(f"\033[31m{self.name}\033[0m의" + "\033[96m{통상공격}\033[0m" "으로" +
+              f"\033[32m{player_name}\033[0m" + f"에게 \033[95m{damage}\033[0m의 데미지를 입혔습니다.")
+        if enemy.hp == 0:
+            print(f"\033[32m{player_name}\033[0m가(이) 쓰러졌습니다.")
+            self.resurrect()
+
+    #         player.resurrect()
+    # def resurrect(self):
+    #     pass
+    def resurrect(self):
+        choice = int(
+            input("쓰러진 당신에게 가호가 내려집니다. 제발 저희를 구원해주세요. (1: 부활 / 2: 안식 )"))
+        if choice == 1:
+            player.hp = player.max_hp
+            print(f"\033[31m{player_name}\033[0m 님 감사합니다. 다시 한번 구원의 손길을!")
+            player.resurrect()
+        else:
+            print("안식을 선택하셨군요. 무리한 부탁을 들어주시느라 수고하셨습니다. 당신을 나무라지 않겠습니다. 부디 편히 잠드시길..")
+
+
 # 캐릭터와 몬스터를 생성합니다. 플레이어의 캐릭터는 이름을 입력받고 몬스터는 랜덤 초이스로 리스트에서 랜덤하게 생성됩니다.
 player_name = input("캐릭터를 생성합니다. 이름을 입력하십시오. : ")
 print(f"하느님 감사합니다, 드디어 돌아오셨군요! " +
       f"\033[32m{player_name}\033[0m" + "님 당신이 떠난 후로 많은 게 변했습니다. ")
-player = Player(player_name, 300, 200, 100, 200)
+player = Player(player_name, 300, 200, 200, 1000)
 devils = [
-    Monster("베리알", 300, 30),
-    Monster("아즈모단", 500, 50),
-    Monster("바알", 1000, 30),
-    Monster("메피스토", 1500, 30),
-    Monster("두리엘", 2000, 20),
-    Monster("안다리엘", 3000, 25),
-    Monster("디아블로", 4000, 30)
+    # Monster("베리알", 3000, 30),
+    # Monster("아즈모단", 5000, 50),
+    # Monster("바알", 4000, 30),
+    # Monster("메피스토", 3500, 30),
+    # Monster("두리엘", 6000, 20),
+    # Monster("안다리엘", 3000, 25),
+    Diablo("디아블로", 20000, 30)
 ]
 devil = random.choice(devils)
 print("불타오르는 대지 그 너머 당신의 눈 앞에 힘의 문양이 떠올랐습니다.")
@@ -120,7 +160,7 @@ while True:
     # 공격 방식 선택
     print("공격 방법을 선택해주세요.")
     select_attack = int(
-        input("1."+"\033[96m{통상공격}\033[0m" + "2." + "\033[93m{마법공격}\033[0m"))
+        input("1."+"\033[96m{통상공격}\033[0m" + "2." + "\033[93m{신성마법}\033[0m"))
 
     # #공격후 스테이터스 확인
     if select_attack == 1:
